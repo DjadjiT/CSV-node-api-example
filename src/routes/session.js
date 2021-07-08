347aeb1d815fbf3924e7944df947c19449c5c6f6
DjadjiT <djadji.traore75@gmail.com>
2021-07-08 13:01:26 +0200

    jai faim

--- src/routes/session.js
+++ src/routes/session.js
@@ -4,26 +4,34 @@ import { Parser } from 'json2csv';
 const router = Router();
 const axios = require('axios')
 
-router.post('/:blackBoxName/:modelId', (req, res) => {
+router.post('/:blackBoxName/:modelId', async (req, res) => {
   let body = req.body
   let url = "http://techsprint-etu.cloudbdf.fr/blackbox_"+req.params.blackBoxName.toLocaleUpperCase()+"/model_"+req.params.modelId
+  let promises = []
+  let data = []
+  body.forEach(async (element) => {
+    promises.push(axios.post(
+      url, 
+      element, 
+      { headers: {
+        'Content-Type': 'application/json',
+        'Authorization': "Basic dGVhbV9lMzppREJiRkZwdA=="
+      }}))
+  });
 
-  axios.post(
-    url, 
-    body, 
-    { headers: {
-      'Content-Type': 'application/json',
-      'Authorization': "Basic dGVhbV9lMzppREJiRkZwdA=="
-    }}).then((res)=>{
-      let json = res.data
-      const json2csv = new Parser();
-      const csv = json2csv.parse(json);
-      res.header('Content-Type', 'text/csv');
-      res.attachment("tab.csv");
-      return res.send(csv);
-  }).catch((err)=>{
-    return res.status(400).send(err.response.data)
+  Promise.all(promises).then((results) => {
+    results.forEach(e=> data.push(e.data))
+    
+    const json2csv = new Parser();
+    const csv = json2csv.parse(data);
+    res.header('Content-Type', 'text/csv');
+    res.attachment("tab.csv");
+    return res.send(csv);
   })
+  .catch((e) => {
+      return res.status(400).send(e)
+  });
+
 });
 
 export default router;
